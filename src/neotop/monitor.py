f7547477ce3358fc07fe6ba15551169d5fcb4207
hassankhizra97-crypto <224271569+hassankhizra97-crypto@users.noreply.github.com>
2026-08-09 14:58:51 +0500

    monitor memoery usage

--- src/neotop/monitor.py
+++ src/neotop/monitor.py
@@ -7,5 +7,11 @@ def monitor_cpu_usage():
         print(f"Aggregate CPU Usage: {aggregate_cpu}%")
         print(f"Per Core CPU Usage: {per_core_cpu}%")
 
-
+def monitor_memory_usage():
+    while True:
+        memory_info = psutil.virtual_memory()
+        print(f"Total Memory: {memory_info.total / (1024 ** 3):.2f} GB")
+        print(f"Available Memory: {memory_info.available / (1024 ** 3):.2f} GB")
+        print(f"Used Memory: {memory_info.used / (1024 ** 3):.2f} GB")
+        print(f"Memory Usage: {memory_info.percent}%")
 
