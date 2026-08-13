7c7055018415db09ad18aada7c096cac07e48a46
hassankhizra97-crypto <224271569+hassankhizra97-crypto@users.noreply.github.com>
2026-08-13 19:06:55 +0500

    styled the dsiaply

--- src/neotop/widgets/core.py
+++ src/neotop/widgets/core.py
@@ -24,7 +24,7 @@ class CoreWidget(Widget):
             for core_number in range(len(per_core)):
                 row = Horizontal(classes="core-row")
                 core_container.mount(row)
-                row.mount(Label(f"Core {core_number}", classes="core-label"))
+                row.mount(Label(f"Core {core_number}:0.0%", id=f"core-label--{core_number}",classes="core-label"))
                 row.mount(
                     ProgressBar(
                         total=100,
@@ -33,9 +33,21 @@ class CoreWidget(Widget):
                         classes="core-progress",
                     )
                 )
+                core_container.mount(row)
+
             self._num_cores = len(per_core)
 
         for core_number, usage in enumerate(per_core):
-            self.query_one(
-                f"#core-progress-{core_number}", ProgressBar
-            ).update(progress=usage)
+
+            label = self.query_one(
+                f"#core-label--{core_number}",
+                Label,
+            )
+
+            progress_bar = self.query_one(
+                f"#core-progress-{core_number}",
+                ProgressBar,
+            )
+
+            label.update(f"Core {core_number}: {usage:.1f}%")
+            progress_bar.update(progress=usage)
